bcb77a9576c3c36c5eeae2d90e6ccd8c911d97c9
sarathchandra251 <sarathchandra251@gmail.com>
2025-11-23 11:01:52 +0530

    fixed bugs in login auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,55 +1,65 @@
-import React, { useState } from 'react'
-import { supabase } from '../supabase';
+import React, { useState } from "react";
+import { supabase } from "../supabase";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const handleEmailChange = (e: any) => {
+    setEmail(e.target.value);
+  };
 
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
 
+  const handleSignUp = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
 
-    const handleEmailChange = (e : any) => {
-        setEmail(e.target.value);
-    }
+    if (error) alert(error.message);
+    else alert("check your mail for confirmation!");
+  };
 
-    const handlePasswordChange = (e : any) => {
-        setPassword(e.target.value);
-    }
+  const handleSignIn = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
 
-    const handleSignUp = async () => {
-        
-        const {data, error} = await supabase.auth.signUp( {
-            email, password
-        });
-
-        if (error) alert(error.message);
-        else alert('check your mail for confirmation!');
-    }
-
-    const handleSignIn = async () => {
-
-        const {data, error} = await supabase.auth.signInWithPassword( {
-            email, password
-        });
-
-        if (error) alert(error.message);
-        else alert("logged in successfully!");
-    }
+    if (error) alert(error.message);
+    else alert("logged in successfully!");
+  };
 
   return (
     <>
+      <div className="flex justify-center items-center bg-blue-100 p-2 h-screen">
+        <form className="flex flex-col gap-2 justify-center items-center bg-white p-4 rounded shadow">
+          <input
+            type="text"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="example@gmail.com"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="password"
+          />
 
-    <form >
-
-        <input type="text" value={email} onChange={handleEmailChange} />
-        <input type="password" value={password} onChange={handlePasswordChange} />
-
-        <button onClick={handleSignUp} >Sign UP</button>
-        <button onClick={handleSignIn} >Sign In</button>
-
-    </form>
+          <button type="button" onClick={handleSignUp}>
+            Sign UP
+          </button>
+          <button type="button" onClick={handleSignIn}>
+            Sign In
+          </button>
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
